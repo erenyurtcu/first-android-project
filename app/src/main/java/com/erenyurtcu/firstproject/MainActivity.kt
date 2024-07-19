6392c39eb8be9e1d694c343b644a7f7ffa1b3dc6
erenyurtcu <ierenyurtcu@gmail.com>
2024-07-20 00:37:26 +0300

    Initial commit

---
+++ app/src/main/java/com/erenyurtcu/firstproject/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.erenyurtcu.firstproject
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.erenyurtcu.firstproject.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun save(view : View) {
+        binding.textView.text = "Saved!"
+    }
+
+    fun cancel(view : View) {
+        binding.textView.text = "Cancelled!"
+
+    }
+}
